4422efb5fcc255e7224a13f02c59cbc435517a17
Alex Sharples <Alex_Sharples_1@hotmail.com>
2024-05-27 09:38:13 +0100

    Only add ground points to pathing map

--- Scripts/NavGraph.cs
+++ Scripts/NavGraph.cs
@@ -7,9 +7,10 @@ public partial class NavGraph : Node2D
 	public TileMap tileMap;
 
 	[Export]
-	public Rect2 bounds;
+	public Rect2I bounds;
 
 	private int tileSize;
+	private int halfTile;
 	private AStar2D pathing;
 	private long pointsEnd;
 
@@ -18,31 +19,30 @@ public partial class NavGraph : Node2D
 		pathing = new AStar2D();
 
 		tileSize = tileMap.TileSet.TileSize.X;
-		var halfTile = tileSize / 2;
+		halfTile = tileSize / 2;
 
 		var xMax = (int)(bounds.Position.X + bounds.Size.X)/tileSize;
 		var xMin = (int)bounds.Position.X/tileSize;
 		var yMin = (int)bounds.Position.Y/tileSize;
 		var yMax = (int)(bounds.Position.Y + bounds.Size.Y)/tileSize;
 
-		long id = 0;
 		for (int x = xMin; x <= xMax; ++x)
 		{
 			for (int y = yMin; y <= yMax; ++y)
 			{
-				pathing.AddPoint(id, new Vector2(x * tileSize + halfTile, y * tileSize + halfTile));
-				//GD.Print($"Added Point at: {x}, {y}");
-				++id;
+				if (IsGroundPoint(x, y))
+				{
+					pathing.AddPoint(GetId(x, y), new Vector2(x * tileSize + halfTile, y * tileSize + halfTile));
+				}
 			}
 		}
-		pointsEnd = id;
 	}
 
     public override void _Draw()
     {
-		for (int i = 0; i < pointsEnd; ++i)
+		foreach (var id in pathing.GetPointIds())
 		{
-			var pos = pathing.GetPointPosition(i);
+			var pos = pathing.GetPointPosition(id);
 
 			var space = tileMap.GetCellTileData(3, new Vector2I((int)pos.X / tileSize, (int)pos.Y / tileSize), false);
 			if (space == null)
@@ -63,4 +63,48 @@ public partial class NavGraph : Node2D
 			}
 		} 
     }
+	
+	private bool IsGroundPoint(int x, int y)
+	{
+		var space = tileMap.GetCellTileData(3, new Vector2I(x, y), false);
+		if (space == null)
+		{
+			var ground = tileMap.GetCellTileData(3, new Vector2I(x, 1 + y), false);
+			return ground != null;
+		}
+		return false;
+	}
+	
+	private long GetId(int x, int y)
+	{
+		return (long)x * bounds.Size.Y + y;
+		/*
+		
+			0,0 1,0 2,0 3,0 4,0 
+			0,1 1,1 2,1 3,1 4,1
+			0,2 1,2 2,2 3,2 4,2
+			0,3 1,3 2,3 3,3 4,3
+			0,4 1,4 2,4 3,4 4,4	
+		
+			0   5   10  15  20 
+			1   6   11  16  21
+			2   7   12  17  22
+			3   8   13  18  23
+			4   9   14  19  24
+		
+		*/
+	}
+
+	private Vector2I GetCoordFromId(long id)
+	{
+		var y = id % bounds.Size.Y;
+		var x = id / bounds.Size.X;
+		return new Vector2I((int)x, (int)y);
+	}
+
+	private Vector2 GetPosFromId(long id)
+	{
+		var coord = GetCoordFromId(id);
+		return new Vector2I(coord.X * tileSize + halfTile, coord.Y * tileSize + halfTile);
+	}
 }
